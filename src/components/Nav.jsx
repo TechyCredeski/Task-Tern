0995ee96fce987d5ba98e071baf689a7b8fec7a7
Credenski <theonlycredeski@gmail.com>
2023-06-15 01:39:59 +0100

    Nav styling

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Nav() {
+return (
+<nav className="flex items-center justify-between p-3 px-10 w-full">
+    <h1 className="text-lg">Task-Tern</h1>
+    <div className="flex items-center justify-between">
+        <button >Login</button>
+        <button>Sign up</button>
+    </div>
+</nav>
+)
+}
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,11 @@
-
+import Nav from "../components/Nav"
 
 export default function Landing() {
 return (
+    <>
+    <Nav/>
     <h1 className="text-3xl font-bold underline">Landing</h1>
+    </>
+
 )
 }
