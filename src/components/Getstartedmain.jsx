5543ed52157ef120029b62766d3860243fed812c
Credenski <theonlycredeski@gmail.com>
2023-06-16 16:23:23 +0100

    error fix

--- src/components/Getstartedmain.jsx
+++ src/components/Getstartedmain.jsx
@@ -16,7 +16,7 @@ return (
     </div>
     <h4 className="text-3xl font-pacifico text-black mb-7 text-center md:mb-7">Ready to supercharge your productivity? Join thousands of satisfied users and experience the power of TaskTern today. 
         Sign up now and take control of your tasks like a pro!</h4>
-        <button className="p-3 text-lg border border-black rounded-md hover:bg-white md:px-7 md:mx-0 ">Try it for free!</button>
+        <button className="p-3 text-lg border border-black rounded-md hover:bg-black hover:text-purple-400 hover:font-bold md:px-7 md:mx-0 ">Try it for free!</button>
 </div>
 )
 }
--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -9,12 +9,15 @@ return (
 <p className="font-thin text-xs text-center">&copy; 2023 Task-Tern. All rights reserved.</p>
 </section>
 
-<ul className="mt-5 text-center flex justify-between w-9/12 mx-auto flex-col mb-5 md:w-3/12 md:mt-0 md:mb-0 md:text-left">
-    <h5 className="text-xl text-black mb-2 font-bold">Quick Links</h5>
+<ul className="mt-5 text-center w-9/12 mx-auto flex-col mb-5 md:w-3/12 md:mt-0 md:mb-0 md:text-left md:w-1/2">
+    <h5 className="text-xl text-black mb-2 font-bold text-center">Quick Links</h5>
+    <span className=" flex justify-between md:flex-row">
     <li className="mb-1"><p>About</p></li>
     <li className="mb-1"><p>Contact</p></li>
     <li className="mb-1"><p>Features</p></li>
     <li className="font-bold"><p>Get Started</p></li>
+    </span>
+
 </ul>
 <div className="flex flex-col content-center text-center">
 <span className="text-xl font-bold"><a href="https://github.com/TechyCredeski" target="blank">Built by Techy Credeski</a></span>
