a71cdf1ee567f35aae7fcffa1262b17547ef8f88
Credenski <theonlycredeski@gmail.com>
2023-06-16 06:44:54 +0100

    fixed hero responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,14 +3,14 @@
 function Hero() {
 return (
 <div className="w-10/12 m-auto" >
-    <div className="mt-10 flex justify-between">
-        <img src="/src/assets/Images/4871715.jpg" alt="Desk calendar with marked dates 3d cartoon style icon" className="h-96 mr-7 rounded-2xl"/>
+    <div className="mt-6 flex justify-between flex-col md:flex-row md:mt-10">
+        <img src="/src/assets/Images/4871715.jpg" alt="Desk calendar with marked dates 3d cartoon style icon" className="h-64 mb-10 rounded-2xl md:h-96 md:mr-7"/>
         <div className="">
-            <h2 className="text-3xl font-pacifico text-purple-400 mb-9">From chaos to control: Transform your life with our seamless todo app.</h2>
+            <h2 className="text-3xl font-pacifico text-purple-400 mb-7 md:mb-9">From chaos to control: Transform your life with our seamless todo app.</h2>
             <p className="mb-9">Stay organized and supercharge your productivity with TaskTern, the ultimate todo app designed to simplify your life.
                 With its intuitive interface and powerful features, TaskTern helps you manage your tasks, 
                 prioritize your goals, and accomplish more every day.</p>
-            <button className="p-3 px-9 text-lg border border-purple-400 rounded-md hover:bg-purple-400">Get Started</button>
+            <button className="p-3 px-10 text-lg border border-purple-400 rounded-md hover:bg-purple-400 mx-16 md:px-9 md:mx-0">Get Started</button>
         </div>
     </div>
 </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@
 
 export default function Nav() {
 return (
-<nav className="  flex items-center justify-between p-3 px-10 md:w-full">
+<nav className="mt-2  flex items-center justify-between p-3 px-10 md:w-full ">
     <h1 className="text-3xl font-pacifico text-purple-400">Task-Tern</h1>
     <div className="flex items-center justify-between space-x-0.5 ">
         <button className="p-1 px-3 border border-purple-400 rounded-md hover:bg-purple-400">Get Started</button>
