30ae49c8e715304936fd032443227e26f0792f71
Credenski <theonlycredeski@gmail.com>
2023-06-16 05:58:05 +0100

    Hero section done

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,11 @@
 import Nav from "../components/Nav"
+import Hero from "../components/Hero"
 
 export default function Landing() {
 return (
     <div className= "background-color: rgb(255 255 255); font-openSans">
     <Nav/>
-    <h1 className="text-3xl font-bold underline">Landing</h1>
+    <Hero/>
     </div>
 
 )
